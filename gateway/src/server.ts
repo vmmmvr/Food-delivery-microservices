9361932fd4f04d35ccc6a77d74e9abdc47e8af62
vmmmvr <amarbakheit998@gmail.com>
2022-09-25 20:18:19 +0200

    init commit

--- gateway/src/server.ts
+++
@@ -1,8 +0,0 @@
-import app from "./app/app";
-import log from "./utils/logger";
-const config =
-  require("./config/default")["default"][process.env.NODE_ENV || "development"];
-
-app.listen("8000", () => {
-  log.info(`${config.appname} is running on PORT 8000`);
-});
